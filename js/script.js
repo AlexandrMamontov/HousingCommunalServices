072109fb2308fc82179c495933ff75daf06a09a3
alexandr_mamontov <sasham13101999@yandex.ru>
2023-04-09 09:50:26 +0300

    main page ready

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// search
+document.addEventListener('DOMContentLoaded', (e) => {
+  document.getElementById('search-form-open').addEventListener('click', (e) => {
+    document.getElementById('search-form').classList.add('form__show')
+  })
+
+  document.getElementById('search-form-close').addEventListener('click', (e) => {
+    document.getElementById('search-form').classList.remove('form__show')
+  })
+
+  document.getElementById('search-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+  })
+})
+
+// burger-menu
+let burger = document.querySelector('.header__burger');
+let menu = document.querySelector('.header__nav');
+let menuLinks = menu.querySelectorAll('.nav__link')
+
+burger.addEventListener('click',
+  function () {
+    burger.classList.toggle('header__burger--active');
+    menu.classList.toggle('header__nav--active');
+    document.body.classList.toggle('stop-scroll');
+})
+
+menuLinks.forEach(function (el) {
+  el.addEventListener('click', function () {
+    burger.classList.remove('header__burger--active');
+    menu.classList.remove('header__nav--active');
+    document.body.classList.remove('stop-scroll');
+  })
+})
+
+// page-up
+$(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() != 0) {
+      $('#btn-top').fadeIn();
+    } else {
+      $('#btn-top').fadeOut();
+    }
+  });
+  $('#btn-top').click(function () {
+    $('body,html').animate({ scrollTop: 0 }, 700);
+  });
+});
